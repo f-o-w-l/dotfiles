9cd2cca8843da07f75a6a588c588264024fe5351
fowl <fowl@pop-os.localdomain>
2024-04-22 21:16:33 -0500

    feat: add put-to-half commands for quarterwindows

--- Linux/quarterwindows_keybinds.sh
+++ Linux/quarterwindows_keybinds.sh
@@ -2,3 +2,8 @@ dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corn
 dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-ne "['<Alt><Shift>2']"
 dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-sw "['<Alt><Shift>3']"
 dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-corner-se "['<Alt><Shift>4']"
+
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-n "['<Alt><Shift>w']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-e "['<Alt><Shift>d']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-s "['<Alt><Shift>s']"
+dconf write /org/gnome/shell/extensions/com-troyready-quarterwindows/put-to-half-w "['<Alt><Shift>a']"
